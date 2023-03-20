fbd2287d38d3711adf15bc9ef5c27589780502f2
Vaimer <vitaliivaimer83@gmail.com>
2023-03-20 22:07:40 +0100

    .

---
+++ lessen_2/scripts/script.js
@@ -0,0 +1,53 @@
+/*const biggerWord = (word1, word2) => {
+    if (word1.length > word2.length) {
+      return word1;
+    } else {
+      return word2;
+    }
+  }
+  console.log(biggerWord("banana","apple"));*/
+  /*const checkPassword = (password) => {
+    if (password.length >= 7) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  console.log(checkPassword("1234567"))*/
+ /* const findLongestWord = (arr) => {
+    let longestWord = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length > longestWord.length) {
+        longestWord = arr[i];
+      }
+    }
+    return longestWord;
+  };
+  const words = ["apple", "banana", "orange", "pineapple"];
+const longestWord = findLongestWord(words);
+console.log(longestWord);
+
+
+// Задача написать функцию которая принимает 2 аргумента
+    // первый - функция, второй - число
+    // и надо выполнить функцию из первого аргумента столько раз сколько у нас число во втором аргументе
+    // myInterval(() => {}, 10)
+    function myInterval(func, number) {
+      func();
+    }
+    const myFunc = () => {
+      console.log(Math.random());
+    };
+    myInterval(myFunc, 10);
+
+    function Name(newName) {
+      document.body.innerHTML = '<p>${newName}!</p>';
+      
+    }*/
+    const getElement = (tag) => {
+      return document.querySelector(tag);
+    };
+    console.log(getElement("pp"));
+llllllllllmlllll
+hhhhgggghggg,,,,1255554444444855222969
+558869699655511222222j11111hhhhhh111jjhjjhjjj
